d17f938090570ffccc1afcce6f0f92ffe85e2163
Yousef-School <yd187507@gmail.com>
2022-01-23 19:39:30 +0330

    Gallery component added

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,7 +1,35 @@
+import { Fragment } from "react/cjs/react.production.min";
 import "./gallery.scss";
+import Heading from "../heading/Heading";
 
 const Gallery = () => {
-    return ( <section className="gallery"></section> );
-}
- 
+  return (
+    <Fragment>
+      <div className="gallery-heading">
+        <Heading title="گالری" />
+      </div>
+      <section className="gallery">
+        <figure className="gallery-item-1">
+          <img src={`/images/g6.jpg`} alt="" />
+        </figure>
+        <figure className="gallery-item-2">
+          <img src={`/images/g2.jpg`} alt="" />
+        </figure>
+        <figure className="gallery-item-3">
+          <img src={`/images/g3.jpg`} alt="" />
+        </figure>
+        <figure className="gallery-item-4">
+          <img src={`/images/g4.jpg`} alt="" />
+        </figure>
+        <figure className="gallery-item-5">
+          <img src={`/images/g5.jpg`} alt="" />
+        </figure>
+        <figure className="gallery-item-6">
+          <img src={`/images/g7.jpg`} alt="" />
+        </figure>
+      </section>
+    </Fragment>
+  );
+};
+
 export default Gallery;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
+import Gallery from "../../components/gallery/Gallery";
 import Intro from "../../components/intro/Intro";
 import PackageList from "../../components/packageList/PackageList";
 import Services from "../../components/services/Services";
+import "./home.scss"
 
 const Home = () => {
-    return ( <main>
+    return ( <main className="home">
         <Intro />
         <PackageList />
         <Services />
+        <Gallery />
     </main> );
 }
  
